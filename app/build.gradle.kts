24876e2db537a0e1739e59367f01112f67485b67
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-08-13 13:16:26 -0400

    Bump MultiSynth to 1.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "audio.multisynth.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.16"
+        versionCode = 18
+        versionName = "1.17"
     }
 
     buildFeatures { buildConfig = false }
