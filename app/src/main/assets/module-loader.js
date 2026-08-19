5cf6717503c4ebcd1b098482034e91276e2e0cbb
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-08-19 13:53:47 -0400

    Skip retired modules during runtime loading

--- app/src/main/assets/module-loader.js
+++ app/src/main/assets/module-loader.js
@@ -1,8 +1,9 @@
 "use strict";
 (function(global){
-  const I=global.MultiSynth?.ModuleIds;
+  const MS=global.MultiSynth||{},I=MS.ModuleIds,X=MS.ModuleCompatibility;
   if(!I)throw new Error("ModuleIds must load before module-loader");
   for(const ident of Object.values(I.CATALOG)){
+    if(X?.isRetired?.(ident.id))continue;
     document.write('<script src="'+ident.moduleScript+'"><\/script>');
   }
 })(window);
