fdb254de34414083396e25c025d9a7e8b50edad4
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-08-20 04:14:47 -0400

    Remove obsolete compatibility repair shim

--- app/src/main/assets/module-compatibility-repair.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-(function(global){
- const MS=global.MultiSynth=global.MultiSynth||{};
- try{MS.ModuleCompatibility?.registerBuilderPackages?.();MS.ModuleCompatibility?.repairSavedProject?.()}catch(e){console.error("Module compatibility startup repair",e)}
-})(window);
