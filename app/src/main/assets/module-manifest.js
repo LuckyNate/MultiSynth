1adb31496e024b403dfa9b3acfdba51d3445051a
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-09-01 00:44:42 -0400

    Declare Been Served note input

--- app/src/main/assets/module-manifest.js
+++ app/src/main/assets/module-manifest.js
@@ -20,7 +20,7 @@
     [I.BIG_DEAL]:row(I.BIG_DEAL,"granular","#b4232f",["audioInput","audioOutput","pcm"],["pcm","storage"]),
     [I.BIG_MOUTH]:row(I.BIG_MOUTH,"effect","#ff4f87",["audioInput","audioOutput","pcm","mic"],["pcm","mic","storage"]),
     [I.GRAIN_LIQOUR]:row(I.GRAIN_LIQOUR,"granular","#8d5fa8",["audioOutput","generator","noteInput","cvInput","pcm"],["pcm","storage"]),
-    [I.BEEN_SERVED]:row(I.BEEN_SERVED,"effect","#d6a04b",["audioInput","audioOutput"],[]),
+    [I.BEEN_SERVED]:row(I.BEEN_SERVED,"effect","#d6a04b",["audioInput","audioOutput","noteInput"],[]),
     [I.GARAGE_BAND]:row(I.GARAGE_BAND,"effect","#8b8b8b",["audioInput","audioOutput"],[]),
     [I.MASTER_OF_LEVELS]:row(I.MASTER_OF_LEVELS,"utility","#d0d0d0",["audioInput","audioOutput","terminalOutput"],[],{audioRole:"terminal"}),
     [I.DENZELS_EQUALIZER]:row(I.DENZELS_EQUALIZER,"effect","#72b9a7",["audioInput","audioOutput"],[]),
