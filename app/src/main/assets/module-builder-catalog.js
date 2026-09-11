6bc7485d0419514b15e02ae0995b4a563cce49c7
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-09-11 15:51:12 -0400

    Remove obsolete Module Builder catalog

--- app/src/main/assets/module-builder-catalog.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(function(global){
-  const MS=global.MultiSynth=global.MultiSynth||{};
-  const I=MS.ModuleIds,B=MS.ModuleBuilderDefinitions;
-  if(!I||!B)return;
-  function all(){return Object.freeze(I.ALL.map(id=>B.get(id)).filter(Boolean))}
-  function missing(){return Object.freeze(I.ALL.filter(id=>!B.get(id)))}
-  MS.ModuleBuilderCatalog=Object.freeze({all,missing,get complete(){return missing().length===0}});
-})(window);
