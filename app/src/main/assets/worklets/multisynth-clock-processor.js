48ff11a12f08825646e6aae210bba9958c318af9
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-09-12 19:21:11 -0400

    Sleep timebase delivery when transport is idle

--- app/src/main/assets/worklets/multisynth-clock-processor.js
+++ app/src/main/assets/worklets/multisynth-clock-processor.js
@@ -1,15 +1,26 @@
 "use strict";
 
 class MultiSynthClockProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.deliver = false;
+    this.port.onmessage = event => {
+      const msg = event.data || {};
+      if (msg.type === "delivery") this.deliver = msg.active === true;
+    };
+  }
+
   process(inputs, outputs) {
     const output = outputs[0];
     if (output) for (const channel of output) channel.fill(0);
-    this.port.postMessage({
-      type: "timebase",
-      frame: currentFrame,
-      time: currentFrame / sampleRate,
-      sampleRate
-    });
+    if (this.deliver) {
+      this.port.postMessage({
+        type: "timebase",
+        frame: currentFrame,
+        time: currentFrame / sampleRate,
+        sampleRate
+      });
+    }
     return true;
   }
 }
