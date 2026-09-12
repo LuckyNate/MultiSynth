c74722fa25c36c55aa0ffcccd3fe3d1a75cc99a3
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-09-12 02:57:12 -0400

    Register +1 routing modules

--- app/src/main/assets/module-manifest.js
+++ app/src/main/assets/module-manifest.js
@@ -24,6 +24,8 @@
     [I.GARAGE_BAND]:row(I.GARAGE_BAND,"effect","#8b8b8b",["audioInput","audioOutput"],[]),
     [I.MASTER_OF_LEVELS]:row(I.MASTER_OF_LEVELS,"utility","#d0d0d0",["audioInput","audioOutput","terminalOutput"],[],{audioRole:"terminal"}),
     [I.ALCHEMY_MIXER]:row(I.ALCHEMY_MIXER,"mixer","#9a6734",["audioInput","audioOutput","terminalOutput"],[],{audioRole:"terminal",carrierBehavior:"passthrough"}),
+    [I.PLUS_ONE_SPLITTER]:row(I.PLUS_ONE_SPLITTER,"routing","#6ec7ff",["audioInput","audioOutput"],[],{carrierBehavior:"passthrough"}),
+    [I.PLUS_ONE_MERGER]:row(I.PLUS_ONE_MERGER,"routing","#ffb86e",["audioInput","audioOutput"],[],{carrierBehavior:"passthrough"}),
     [I.DENZELS_EQUALIZER]:row(I.DENZELS_EQUALIZER,"effect","#72b9a7",["audioInput","audioOutput"],[]),
     [I.ECHO_CANYON]:row(I.ECHO_CANYON,"effect","#c47b48",["audioInput","audioOutput"],[]),
     [I.CONTROL_FREAK]:row(I.CONTROL_FREAK,"controller","#75b7ff",["noteInput","midi","cvOutput"],["midi"]),
