13423a66e703c1cdc20542f4618c018786dc1c5f
Nate Wyatt <86132136+LuckyNate@users.noreply.github.com>
2026-09-17 16:00:31 -0400

    Remove obsolete ladder synth manifest entry

--- app/src/main/assets/module-manifest.js
+++ app/src/main/assets/module-manifest.js
@@ -35,7 +35,6 @@
     [I.TEST_MODULE]:row(I.TEST_MODULE,"utility","#d8d8d8",[]),
     [I.PURE_SYNTH]:row(I.PURE_SYNTH,"instrument","#f4f4f0",["audioInput","audioOutput","generator","noteInput"],[]),
     [I.QUAD_SYNTH]:row(I.QUAD_SYNTH,"instrument","#ffb000",["audioInput","audioOutput","generator","noteInput"],[]),
-    [I.LADDERSYNTH]:row(I.LADDERSYNTH,"instrument","#58ff78",["audioInput","audioOutput","generator","noteInput"],["midi"]),
     [I.HOOK_AND_LADDER]:row(I.HOOK_AND_LADDER,"instrument","#d7261e",["audioOutput","generator","noteInput"],["midi"]),
     [I.NO_QUARTER]:row(I.NO_QUARTER,"instrument","#77a4ff",["audioInput","audioOutput","generator","noteInput"],[]),
     [I.RANDRONE]:row(I.RANDRONE,"generator","#9efcff",["audioInput","audioOutput","generator","noteInput","clockFollower"],["midi"]),
